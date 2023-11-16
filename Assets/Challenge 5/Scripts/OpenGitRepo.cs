d83bed637ca0c48b76daaca67591b84288547318
Matamenas <B00149694@mytudublin.ie>
2023-11-16 17:10:05 +0000

    Upload Work Without Build

---
+++ Assets/Challenge 5/Scripts/OpenGitRepo.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OpenGitRepo : MonoBehaviour
+{
+    public void OpenURL()
+    {
+        Application.OpenURL("https://github.com/Matamenas/Challange5_B00149694");
+    }
+}
